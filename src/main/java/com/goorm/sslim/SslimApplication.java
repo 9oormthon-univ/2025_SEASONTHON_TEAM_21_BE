d641d1e7569dea66154b9ad389f35ffaaaedac45
SungYeon <yousung1018@naver.com>
2025-09-01 22:53:56 +0900

    Setting : Springboot init

---
+++ src/main/java/com/goorm/sslim/SslimApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.sslim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goorm/sslim/SslimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.sslim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
