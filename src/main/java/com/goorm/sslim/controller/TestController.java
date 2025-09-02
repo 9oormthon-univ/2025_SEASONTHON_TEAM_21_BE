e2eff74e15a2a225d64eb49675ef4b6927cc8c8d
leedy5521 <leedy5521@naver.com>
2025-09-02 15:47:19 +0900

    Test: test용 컨트롤러, 예외 추가

---
+++ src/main/java/com/goorm/sslim/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.goorm.sslim.controller;
+
+import com.goorm.sslim.global.code.ErrorCode;
+import com.goorm.sslim.global.code.ResponseCode;
+import com.goorm.sslim.global.exception.TestException;
+import com.goorm.sslim.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/excute")
+    public ApiResponse<Void> test(@RequestParam String error) {
+        if(error.equals("yes")) {
+            System.out.println("------------------------------");
+            throw new TestException(ErrorCode._BAD_REQUEST);
+        }
+
+        return ApiResponse.onSuccess(ResponseCode.OK, null);
+    }
+}
