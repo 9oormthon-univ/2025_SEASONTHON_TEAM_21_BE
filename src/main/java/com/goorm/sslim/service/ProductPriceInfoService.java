6b9d50bdce7954414f6cd3c61ea03a19bb4e3a43
leedy5521 <leedy5521@naver.com>
2025-09-03 23:15:34 +0900

    Feat: 생필품 물가 api service 생성

---
+++ src/main/java/com/goorm/sslim/service/ProductPriceInfoService.java
@@ -0,0 +1,53 @@
+package com.goorm.sslim.service;
+
+import com.goorm.sslim.foodcost.dto.response.ProductPriceInfoDTO;
+import com.goorm.sslim.foodcost.dto.response.ProductPriceInfoResponse;
+import com.goorm.sslim.global.code.ErrorCode;
+import com.goorm.sslim.global.exception.GeneralException;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.StringReader;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductPriceInfoService {
+
+    @Value("${spring.openapi.product-price.service-key}")
+    private String serviceKey;
+
+    private static final String BASE_URL =
+            "http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do";
+
+    public List<ProductPriceInfoDTO> fetchProductPriceInfo(String goodInspectDay, String goodId) throws JAXBException {
+        // 파라미터 구성
+        String url = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                .queryParam("goodInspectDay", goodInspectDay)
+                .queryParam("goodId", goodId)
+                .queryParam("ServiceKey", serviceKey)
+                .toUriString();
+
+        // 1. RestTemplate으로 XML String 받기
+        RestTemplate restTemplate = new RestTemplate();
+        String xmlResponse = restTemplate.getForObject(url, String.class);
+        if(xmlResponse == null) {
+            throw new GeneralException(ErrorCode._NULL_API);
+        }
+
+
+        // 2. JAXB로 XML -> DTO List로 변환
+        JAXBContext jaxbContext = JAXBContext.newInstance(ProductPriceInfoResponse.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        ProductPriceInfoResponse response =
+                (ProductPriceInfoResponse) unmarshaller.unmarshal(new StringReader(xmlResponse));
+
+        return response.getItems();
+    }
+}
