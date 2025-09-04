bc0c4d8678a0c7bc09ae66b6bec70df22c00ed9f
leedy5521 <leedy5521@naver.com>
2025-09-04 15:57:50 +0900

    Test: api 호출 및 저장테스트 컨트롤러 작성

--- src/main/java/com/goorm/sslim/controller/TestController.java
+++ src/main/java/com/goorm/sslim/controller/TestController.java
@@ -1,25 +1,28 @@
 package com.goorm.sslim.controller;
 
-import com.goorm.sslim.global.code.ErrorCode;
+import com.goorm.sslim.foodcost.dto.response.ProductPriceInfoDTO;
 import com.goorm.sslim.global.code.ResponseCode;
-import com.goorm.sslim.global.exception.TestException;
 import com.goorm.sslim.global.response.ApiResponse;
+import com.goorm.sslim.service.ProductPriceInfoService;
+import jakarta.xml.bind.JAXBException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/execute")
-    public ApiResponse<Void> test(@RequestParam String error) {
-        if(error.equals("yes")) {
-            throw new TestException(ErrorCode._BAD_REQUEST);
-        }
+    private final ProductPriceInfoService productPriceInfoService;
+
+    @GetMapping("/prices")
+    public ApiResponse<Void> test(@RequestParam String goodInspectDay, String goodId) throws JAXBException {
+        productPriceInfoService.calculateAndSaveAverage(goodInspectDay, goodId);
 
         return ApiResponse.onSuccess(ResponseCode.OK, null);
     }
