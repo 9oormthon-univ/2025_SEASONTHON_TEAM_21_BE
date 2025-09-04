a1a8ca41ce3515926ec065782d2a43f2568a9c63
leedy5521 <leedy5521@naver.com>
2025-09-05 01:46:25 +0900

    fix: FoodCostRepository 간단 수정

--- src/main/java/com/goorm/sslim/foodcost/repository/FoodCostRepository.java
+++ src/main/java/com/goorm/sslim/foodcost/repository/FoodCostRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodCostRepository extends JpaRepository<FoodCost, Long> {
+public interface FoodCostRepository extends JpaRepository<FoodCost, String> {
 
 }
