c29c1c68e7e535bbe02c1d73ab68287215542d0b
leedy5521 <leedy5521@naver.com>
2025-09-04 13:16:07 +0900

    Fix: ProductPriceInfoResponse 수정

--- src/main/java/com/goorm/sslim/foodcost/dto/response/ProductPriceInfoResponse.java
+++ src/main/java/com/goorm/sslim/foodcost/dto/response/ProductPriceInfoResponse.java
@@ -1,15 +1,34 @@
 package com.goorm.sslim.foodcost.dto.response;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@XmlRootElement(name = "response")
+@XmlAccessorType(XmlAccessType.FIELD)
 @Getter
-@XmlRootElement(name = "result")
+@Setter
+@NoArgsConstructor
 public class ProductPriceInfoResponse {
 
-    @XmlElement
-    private List<ProductPriceInfoDTO> items;
+    @XmlElement(name = "result")
+    private Result result;
+
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Result {
+
+        @XmlElement(name = "iros.openapi.service.vo.goodPriceVO")
+        private List<ProductPriceInfoDTO> items;
+    }
 }
